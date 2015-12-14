71c4e047cc52ca73ea28dfae8f6cd750d5b707cd
Ryan Yep <ryan.yep@gmail.com>
2015-12-14 17:04:47 -0600

    Add paperclip gem to Gemfile

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ gem 'rails', '4.2.4'
 # Use postgresql as the database for Active Record
 gem 'pg'
 gem 'bootstrap-sass', '~> 3.3.6'
+gem "paperclip", "~> 4.3"
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :configure_permitted_parameters, :if => :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller? #http://stackoverflow.com/questions/29710717/undefined-local-variable-or-method-resource-class-in-devise
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :email << :password
