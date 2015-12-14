a932f129898bf07a6382e96a89b7ea3310de560d
Ryan Yep <ryan.yep@gmail.com>
2015-12-14 15:50:34 -0600

    Ran migration after creating Players model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151210233014) do
+ActiveRecord::Schema.define(version: 20151214214811) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "players", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.date     "DOB"
+    t.string   "position"
+    t.string   "college"
+    t.date     "debut"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
