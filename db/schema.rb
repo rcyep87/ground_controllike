a69f986b217312d0a1fdce27304d2cdfa5c8c983
Ryan Yep <ryan.yep@gmail.com>
2015-12-19 09:00:43 -0600

    Added height attribute to players table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151218021127) do
+ActiveRecord::Schema.define(version: 20151218022311) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20151218021127) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "throws"
+    t.string   "height"
   end
 
   create_table "users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,13 @@
-Player.create({
-
-      first_name: "Carlos",
-      last_name:  "Correa",
-      DOB:        "1994-09-22",
-      Position:   "SS",
-      College:    "N/A",
-      Throws:     "Right"
-
-  })
+# Player.create({
+#
+#       first_name: "Carlos",
+#       last_name:  "Correa",
+#       DOB:        "1994-09-22",
+#       Position:   "SS",
+#       College:    "N/A",
+#       Throws:     "Right"
+#
+#   })
 
 Player.create({
 
@@ -20,3 +20,28 @@ Player.create({
       Throws:     "Left"
 
   })
+
+Player.create({
+
+      first_name: "Lance",
+      last_name:  "McCullers",
+      DOB:        "1993-10-02",
+      Position:   "P",
+      College:    "Jesuit (FL) HS",
+      Debut:      "2015-05-18",
+      Throws:     "Right"
+
+  })
+
+  Player.create({
+
+        first_name: "Jose",
+        last_name:  "Altuve",
+        DOB:        "1990-05-06",
+        Position:   "2B",
+        College:    "N/A",
+        Debut:      "2011-07-20",
+        Throws:     "Right",
+        height:     "5'6""
+
+    })
