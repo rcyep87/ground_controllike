00ad05c7dcc9d1c325c80af6b110c5336e71e374
Ryan Yep <ryan.yep@gmail.com>
2015-12-19 20:24:29 -0600

    Updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,24 @@
-# Player.create({
-#
-#       first_name: "Carlos",
-#       last_name:  "Correa",
-#       DOB:        "1994-09-22",
-#       Position:   "SS",
-#       College:    "N/A",
-#       Throws:     "Right",
-#       height:     "",
-#       team_id:    1
-#
-#   })
+Player.create({
+
+      first_name: "Carlos",
+      last_name:  "Correa",
+      DOB:        "1994-09-22",
+      position:   "SS",
+      college:    "N/A",
+      throws:     "Right",
+      team_id:    1
+
+  })
 
 Player.create({
 
       first_name: "Dallas",
       last_name:  "Keuchel",
       DOB:        "1988-01-01",
-      Position:   "P",
-      College:    "Arkansas",
-      Debut:      "2012-06-17",
-      Throws:     "Left",
-      height:     "",
+      position:   "P",
+      college:    "Arkansas",
+      debut:      "2012-06-17",
+      throws:     "Left",
       team_id:    1
 
   })
@@ -30,11 +28,10 @@ Player.create({
       first_name: "Lance",
       last_name:  "McCullers",
       DOB:        "1993-10-02",
-      Position:   "P",
-      College:    "Jesuit (FL) HS",
-      Debut:      "2015-05-18",
-      Throws:     "Right"
-      height:     "",
+      position:   "P",
+      college:    "Jesuit (FL) HS",
+      debut:      "2015-05-18",
+      throws:     "Right",
       team_id:    1
 
   })
@@ -44,11 +41,10 @@ Player.create({
         first_name: "Jose",
         last_name:  "Altuve",
         DOB:        "1990-05-06",
-        Position:   "2B",
-        College:    "N/A",
-        Debut:      "2011-07-20",
-        Throws:     "Right",
-        height:     "5'6",
+        position:   "2B",
+        college:    "N/A",
+        debut:      "2011-07-20",
+        throws:     "Right",
         team_id:    1
 
     })
