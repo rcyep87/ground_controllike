e94136aa476f2629f0c643c98359c9ac8d7ffea5
Ryan Yep <ryan.yep@gmail.com>
2015-12-22 23:24:07 -0600

    Add 30 min timeout for logged in users who are not admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,14 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :timeoutable
+
+   def timeout_in
+     if self.admin?
+       1.year
+     else
+       30.minutes
+     end
+   end
+   
 end
