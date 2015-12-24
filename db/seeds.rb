2483f65ef630f7f077376900a2bae552fc71769b
Ryan Yep <ryanyep@Ryans-MacBook.local>
2015-12-24 16:32:03 -0600

    Removed traces of turbolinks to see if it fixes navbar not showing on home controller issue and updated seed file to include enough players to test search bar

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
+Team.create({
+      team_name: "Houston Astros"
+  })
+
 Player.create({
 
       first_name: "Carlos",
@@ -44,6 +48,136 @@ Player.create({
         position:   "2B",
         college:    "N/A",
         debut:      "2011-07-20",
+        throws:     "Right",
+        team_id:    1
+
+    })
+
+  Player.create({
+
+        first_name: "Josh",
+        last_name:  "Fields",
+        DOB:        "1985-08-19",
+        position:   "P",
+        college:    "Georgia",
+        debut:      "2013-04-02",
+        throws:     "Right",
+        team_id:    1
+
+    })
+
+  Player.create({
+
+        first_name: "Luke",
+        last_name:  "Gregerson",
+        DOB:        "1984-05-14",
+        position:   "P",
+        college:    "St. Xavier, IL",
+        debut:      "2009-04-06",
+        throws:     "Right",
+        team_id:    1
+
+    })
+
+  Player.create({
+
+        first_name: "Jason",
+        last_name:  "Castro",
+        DOB:        "1987-06-18",
+        position:   "C",
+        college:    "Stanford",
+        debut:      "2010-06-22",
+        throws:     "Right",
+        team_id:    1
+
+    })
+
+  Player.create({
+
+        first_name: "George",
+        last_name:  "Springer",
+        DOB:        "1989-09-19",
+        position:   "OF/RF",
+        college:    "Connecticut",
+        debut:      "2014-04-16",
+        throws:     "Right",
+        team_id:    1
+
+    })
+
+  Player.create({
+
+        first_name: "Carlos",
+        last_name:  "Gomez",
+        DOB:        "1985-12-04",
+        position:   "OF/CF",
+        college:    "N/A",
+        debut:      "2007-05-13",
+        throws:     "Right",
+        team_id:    1
+
+    })
+
+  Player.create({
+
+        first_name: "Preston",
+        last_name:  "Tucker",
+        DOB:        "1990-07-06",
+        position:   "OF",
+        college:    "Florida",
+        debut:      "2015-05-07",
+        throws:     "Left",
+        team_id:    1
+
+    })
+
+  Player.create({
+
+        first_name: "Jake",
+        last_name:  "Marisnick",
+        DOB:        "1991-03-30",
+        position:   "OF",
+        college:    "N/A",
+        debut:      "2013-07-23",
+        throws:     "Right",
+        team_id:    1
+
+    })
+
+  Player.create({
+
+        first_name: "Colby",
+        last_name:  "Rasmus",
+        DOB:        "1986-08-11",
+        position:   "OF",
+        college:    "N/A",
+        debut:      "2009-04-07",
+        throws:     "Left",
+        team_id:    1
+
+    })
+
+  Player.create({
+
+        first_name: "Evan",
+        last_name:  "Gattis",
+        DOB:        "1986-08-18",
+        position:   "DH/OF",
+        college:    "Texas - Permian Basin",
+        debut:      "2013-04-03",
+        throws:     "Right",
+        team_id:    1
+
+    })
+
+  Player.create({
+
+        first_name: "Mike",
+        last_name:  "Fiers",
+        DOB:        "1985-06-15",
+        position:   "P",
+        college:    "Nova Southeastern",
+        debut:      "2011-09-14",
         throws:     "Right",
         team_id:    1
 
